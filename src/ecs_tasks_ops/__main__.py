91ff05ce049e0b760ab47937e17873c66efefe3c
Pablo Palazon <pablo.palazon@gmail.com>
2020-10-08 20:28:45 +0200

    Improve click context and error handling

--- src/ecs_tasks_ops/__main__.py
+++ src/ecs_tasks_ops/__main__.py
@@ -7,13 +7,16 @@ from . import pretty_table
 @click.group()
 @click.option('--debug/--no-debug', default=False)
 @click.version_option()
-def main(debug) -> None:
+@click.pass_context
+def main(ctx, debug) -> None:
     """Ecs Tasks Ops."""
-    pass
+    ctx.ensure_object(dict)
+    ctx.obj['DEBUG'] = debug
 
 
 @main.command()
-def cluster():
+@click.pass_context
+def cluster(ctx):
     """Clusters information."""
     click.secho("Getting list of ECS cluster", fg="green")
     clusters = ecs_facade.get_cluster_list()
@@ -22,12 +25,17 @@ def cluster():
 
 @main.command()
 @click.option('-c', '--cluster', default='default')
-def services(cluster):
+@click.pass_context
+def services(ctx, cluster):
     """Services defined in a cluster."""
     click.secho(f"Getting list of Services for '{cluster}'", fg="green")
-    services = ecs_facade.get_services(cluster)
-    click.echo(pretty_table.tabulate_list_json_keys(services, ['serviceArn', 'serviceName', 'status', 'runningCount',
-                                                               'desiredCount']))
+    try:
+        services = ecs_facade.get_services(cluster)
+        click.echo(pretty_table.tabulate_list_json_keys(
+            services, ['serviceArn', 'serviceName', 'status', 'runningCount', 'desiredCount']))
+    except ecs_facade.ecs_client.exceptions.ClusterNotFoundException:
+        click.secho(f"Cluster {cluster} not found", fg="red")
+        return []
 
 
 if __name__ == "__main__":
